ecfe41fe0b72dc4e0b3e3d5d6a882ed36e4706c2
Kyle Luke <luke.kylew@gmail.com>
2020-10-08 20:43:02 -0600

    Added index test to ask if redux store is set up to pass actions from dispatch to reducer.

--- frontend/src/tests/Index.test.js
+++ frontend/src/tests/Index.test.js
@@ -4,16 +4,20 @@ import thunk from "redux-thunk";
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-// allows us to easily return reponses and/or success/fail for a thunk that calls a service
-const mockServiceCreator = (body, succeeds = true) => () =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => (succeeds ? resolve(body) : reject(body)), 10);
-  });
-  
-  describe('authenticate action', () => {
-    let store;
-    // set up a fake store for all our tests
-    beforeEach(() => {
-      store = mockStore({ posts: [] });
-    })
-});
+// You would import the action from your codebase in a real scenario
+const addPost = () => ({ type: 'ADD_POST' })
+ 
+it('should dispatch action', () => {
+ 
+  // Initialize mockstore with empty state
+  const initialState = {}
+  const store = mockStore(initialState)
+ 
+  // Dispatch the action
+  store.dispatch(addPost())
+ 
+  // Test if your store dispatched the expected actions
+  const actions = store.getActions()
+  const expectedPayload = { type: 'ADD_POST' }
+  expect(actions).toEqual([expectedPayload])
+})
